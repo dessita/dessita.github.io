77bb4907e3a1b10300a766b87e38e3c033d5aeeb
Desislava Slavkova <dessita@gmail.com>
2020-06-16 12:20:29 -0400

    updated bootstrap version

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage(
-        {navigation:true,
-         anchors:['home','about','projects-link', 'money-matcher-slide','finclear-consumer-slide', 'power-wallet-slide','financial-apps-slide','dynamic-dashboard-slide','eav-slide','contact']
-           });
-});
