12088445a355a7f26ce1adf4c1f33ee86e497dbd
Desislava Slavkova <dessita@gmail.com>
2021-02-20 23:26:59 -0500

    first version

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+
+//= require jquery
+//= require popper
+//= require util
+//= require tooltip
+//= require alert
+//= require button
+//= require collapse
+//= require dropdown
+
+//= require main
+
